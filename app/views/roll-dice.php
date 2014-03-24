aeecc5f9367245c36ede2bd6e46c28106800e9d6
cameronholland90 <cameron@thenearsky.com>
2014-03-24 14:34:08 -0500

    reformatted what was echoed in view

--- app/views/roll-dice.php
+++ app/views/roll-dice.php
@@ -3,7 +3,7 @@
 	<title>Roll Dice</title>
 </head>
 <body>
-	<h1><?= "Random Number: " . $rand . "</h1><h1> Your Guess: " . $guess ?></h1>
+	<h1>Random Number: <?= $rand; ?></h1><h1>Your Guess: <?= $guess; ?></h1>
 	<h2><?= $result; ?></h2>
 </body>
 </html>
