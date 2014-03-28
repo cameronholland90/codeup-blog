3315c9872bc01df6bb9a253ef88221dee1b3d4b7
cameronholland90 <cameron@thenearsky.com>
2014-03-28 11:16:21 -0500

    put created_at formatting on specific post view

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -6,7 +6,7 @@
 @section('content')
     <div class='container main-container'>
     	<h1>Specific Post</h1>
-    	<h3>{{{ $post->title }}}</h3>
+    	<h3>{{{ $post->title }}}<br><small>Posted on {{ $post->created_at->setTimezone('America/Chicago')->format('l, F jS Y @ h:i:s A'); }}</small></h3>
     	<p> {{{ $post->body }}} </p>
 
     	<a href="#" id='btnDeletePost'>Delete</a> | <a href="{{{ action('PostsController@edit', $post->id) }}}">Edit</a><br>
