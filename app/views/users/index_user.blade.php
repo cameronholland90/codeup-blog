fe3fafb287a1df47a513ec46ab046c02960a3fc8
cameronholland90 <cameron@thenearsky.com>
2014-04-10 13:18:25 -0500

    allow admin to delete users

--- app/views/users/index_user.blade.php
+++ app/views/users/index_user.blade.php
@@ -30,13 +30,34 @@
                 <td>{{{ $user->last_name }}}</td>
                 <td>{{{ $user->username }}}</td>
                 <td>{{{ $user->email }}}</td>
-                <td><a href="{{ action('') }}"></a></td>
+                <td>
+                    <a href="#" class='btnDeletePost btn btn-danger' data-userid="{{{$user->id}}}">Delete</a>
+                    
+                </td>
             </tr>
     	@endforeach
         </table>
-    	{{ $users->links() }} <br>
+        {{ $users->appends(array('user_search' => Input::get('user_search')))->links() }} <br>
+        
         @if (Auth::check())
     	   <a href="{{{ action('UsersController@create') }}}" class='btn btn-success'>Create New User</a>
         @endif
+
+        {{ Form::open(array('action' => array('UsersController@destroy'), 'method' => 'delete', 'id' => 'formDeletePost')) }}
+        {{ Form::close() }}
+
+@stop
+
+@section('bottom-script')
+    <script>
+        $('.btnDeletePost').on('click', function(e) {
+            var user = $(this).data('userid');
+            e.preventDefault();
+            if (confirm('Are you sure you want to delete this user?')) {
+                $("#formDeletePost").attr('action', '/users/' + user);
+                $('#formDeletePost').submit();
+            };
+        });
+    </script>
 
 @stop
